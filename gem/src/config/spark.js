67284b2c367d2b4280cc3aed0c4d0a1aca79b004
abhilash0023 <abikumar0023@gmail.com>
2025-04-01 20:15:57 +0530

    Update spark.js

--- gem/src/config/spark.js
+++ gem/src/config/spark.js
@@ -8,7 +8,7 @@ import {
   } from "@google/generative-ai"
   
   const MODEL_NAME = "gemini-1.0-pro";
-  const API_KEY = "AIzaSyCJC2iM1OVNb-bjIbfYrHy6ohyGCXAxFnA";
+  const API_KEY = "ADD YOUR API KEY";
   
   async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
